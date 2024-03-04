c1be2aed9a249c05783be285da7378e90ccf0eae
silvertxd <silvertxd@gmail.com>
2024-03-04 23:43:56 +0200

    moved test products to another folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
-import path from 'path'
+import { promises as fs } from 'fs';
+import Products from './ui/products';
+interface Product {
+	id: number;
+	name: string;
+	price: number;
+	description: string;
+	image_url: string;
+}
 
 export default async function Home() {
-  
-	return <div>
-    hELLO
-  </div>;
+	const file = await fs.readFile(
+		process.cwd() + '/public/products.json',
+		'utf8'
+	);
+	const data: Product[] = JSON.parse(file);
+
+	return (
+			<div className='relative mx-auto my-5 flex max-w-6xl flex-col px-4 py-5 sm:px-6 lg:px-8'>
+				<Products products={data} />
+			</div>
+
+	);
 }
