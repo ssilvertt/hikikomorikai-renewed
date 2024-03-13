80295b48b3e4bb4681a1da536955bef77f5bebc1
silvertxd <silvertxd@gmail.com>
2024-03-13 02:03:10 +0200

    Update web.php

--- hikikomori-backend/routes/web.php
+++ hikikomori-backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+
 
 require __DIR__.'/auth.php';
