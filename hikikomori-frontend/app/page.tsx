609bcc47c1c63d2ce5cd88166adf9282a5780b47
silvertxd <silvertxd@gmail.com>
2024-03-20 22:46:38 +0200

    using server component at main page

--- hikikomori-frontend/app/page.tsx
+++ hikikomori-frontend/app/page.tsx
@@ -1,22 +1,50 @@
-import Products from '@/components/products';
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-axios.defaults.withXSRFToken = true;
+import { ApiProduct } from '@/lib/definitions';
+import Image from 'next/image';
+import Link from 'next/link';
 
-interface ApiProduct {
-	id: number;
-	name: string;
-	description: string;
-	price: string;
-	image: string;
-	created_at: string;
-	updated_at: string;
+async function getProducts() {
+	const res = await fetch('http://127.0.0.1:8000/api/products', {cache: 'force-cache'});
+
+	if (!res.ok) {
+		throw new Error('Failed to fetch data');
+	}
+
+	return res.json();
 }
 
-export default function Home() {
+export default async function Home() {
+	const products: ApiProduct[] = await getProducts();
+
 	return (
 		<>
-			<Products />
+			<div className='relative z-20'>
+				<div>
+					<div className='relative mx-auto my-20 flex max-w-7xl flex-col px-4 py-5 sm:px-6 lg:px-8'>
+						<div className='flex flex-wrap justify-center gap-20'>
+							{products.map((product, index) => (
+								<Link key={index} href={`/product/${product.id}`}>
+									<div
+										key={index}
+										className='font-raleway item group flex flex-col items-center w-72'
+									>
+										<Image
+											width={250}
+											height={250}
+											src={`http://localhost:8000/storage/${product.images[0].image_path}`}
+											alt={product.name}
+											className='h-64 w-full max-w-[250px] object-contain'
+											unoptimized={true}
+										/>
+										<h3 className='mt-10 text-l group-hover:text-red-700'>
+											{product.name}
+										</h3>
+									</div>
+								</Link>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
