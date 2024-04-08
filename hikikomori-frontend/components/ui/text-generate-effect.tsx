f0457668d2ba42f80c50df8a89c1d24a505e2327
silvertxd <silvertxd@gmail.com>
2024-04-09 01:30:03 +0200

    added text generation effect component

---
+++ hikikomori-frontend/components/ui/text-generate-effect.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { useEffect } from 'react';
+import { motion, stagger, useAnimate } from 'framer-motion';
+import { cn } from '@/lib/utils';
+
+export const TextGenerateEffect = ({
+	words,
+	className,
+}: {
+	words: string;
+	className?: string;
+}) => {
+	const [scope, animate] = useAnimate();
+	let wordsArray = words.split(' ');
+	useEffect(() => {
+		animate(
+			'span',
+			{
+				opacity: 1,
+			},
+			{
+				duration: 0.5,
+				delay: stagger(0.2),
+			}
+		);
+	}, [scope.current]);
+
+	const renderWords = () => {
+		return (
+			<motion.div ref={scope}>
+				{wordsArray.map((word, idx) => {
+					return (
+						<motion.span key={word + idx} className="opacity-0">
+							{word}{' '}
+						</motion.span>
+					);
+				})}
+			</motion.div>
+		);
+	};
+
+	return (
+		<div>
+			<div className="mt-4">
+				<div
+					className={cn(
+						'text-center md:text-right text-4xl md:text-4xl  text-red-700 font-raleway',
+						className
+					)}
+				>
+					{renderWords()}
+				</div>
+			</div>
+		</div>
+	);
+};
