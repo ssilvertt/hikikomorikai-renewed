26fb66a900bc30ad983f4cabe1e6acaaa616ae0c
silvertxd <silvertxd@gmail.com>
2024-04-09 01:27:05 +0200

    added image gallery for product page

---
+++ hikikomori-frontend/components/ui/gallery.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { ProductImage } from '@/lib/definitions';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+import LightGallery from 'lightgallery/react';
+import Image from 'next/image';
+
+import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lightgallery.css';
+
+interface GalleryProps {
+	images: ProductImage[];
+}
+
+export default function Gallery({ images }: GalleryProps) {
+	const onInit = () => {};
+
+	return (
+		<LightGallery
+			onInit={onInit}
+			speed={500}
+			plugins={[lgThumbnail, lgZoom]}
+			thumbnail={false}
+			download={false}
+		>
+			{images.map((image, index) => {
+				return (
+					<a
+						key={index}
+						href={`http://localhost:8000/storage/${image.image_path}`}
+					>
+						<Image
+							alt=""
+							src={`http://localhost:8000/storage/${image.image_path}`}
+							width={80}
+							height={80}
+							unoptimized={true}
+							className="h-20 w-20 object-cover rounded-full brightness-50 hover:brightness-125 transition-all
+							shadow-lg shadow-white  "
+						/>
+					</a>
+				);
+			})}
+		</LightGallery>
+	);
+}
