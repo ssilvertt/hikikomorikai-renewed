26c331f0927ff1caab6d2a06a75cf8ff8657a308
silvertxd <silvertxd@gmail.com>
2024-04-09 01:29:46 +0200

    fixed spacing

--- hikikomori-frontend/lib/definitions.ts
+++ hikikomori-frontend/lib/definitions.ts
@@ -1,41 +1,41 @@
 export type ResponseError = {
-  response: {
-    status: number;
-    data: {
-      errors?: { [key: string]: string[] };
-    };
-  };
-}
+	response: {
+		status: number;
+		data: {
+			errors?: { [key: string]: string[] };
+		};
+	};
+};
 
 export type SetErrors = (errors: { [key: string]: string[] }) => void;
 
 export type SetStatus = {
-  (status: string | null): void;
-}
+	(status: string | null): void;
+};
 
 export type User = {
-    id: number
-    name: string
-    email: string
-    email_verified_at?: string
-    created_at: string
-    updated_at: string
-}
+	id: number;
+	name: string;
+	email: string;
+	email_verified_at?: string;
+	created_at: string;
+	updated_at: string;
+};
 
 export type ApiProduct = {
 	id: number;
 	name: string;
 	description: string;
 	price: string;
-	images: Image[];
-	created_at:string;
-	updated_at:string;
-}
+	images: ProductImage[];
+	created_at: string;
+	updated_at: string;
+};
 
-export type Image = {
-  id: number;
-  product_id: number;
-  image_path: string;
-  created_at: string;
-  updated_at: string;
-}
+export type ProductImage = {
+	id: number;
+	product_id: number;
+	image_path: string;
+	created_at: string;
+	updated_at: string;
+};
