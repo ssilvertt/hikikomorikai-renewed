107d6af981755a648295b545a803b0d1b44997e0
silvertxd <silvertxd@gmail.com>
2024-04-09 01:22:27 +0200

    fixed spacing

--- hikikomori-frontend/app/(auth)/AuthSessionStatus.tsx
+++ hikikomori-frontend/app/(auth)/AuthSessionStatus.tsx
@@ -3,16 +3,15 @@ const AuthSessionStatus = ({
 	className,
 	...props
 }: {
-	status: string | null
-	className?: string} & React.HTMLAttributes<HTMLDivElement>): JSX.Element | null => (
-    <>
-        {status && (
-            <div
-                className={`${className} font-medium text-sm`}
-                {...props}>
-                {status}
-            </div>
-        )}
-    </>
-)
-export default AuthSessionStatus
+	status: string | null;
+	className?: string;
+} & React.HTMLAttributes<HTMLDivElement>): JSX.Element | null => (
+	<>
+		{status && (
+			<div className={`${className} font-medium text-sm`} {...props}>
+				{status}
+			</div>
+		)}
+	</>
+);
+export default AuthSessionStatus;
