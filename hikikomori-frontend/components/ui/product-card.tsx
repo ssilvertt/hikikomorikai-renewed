8f0917e6528a68e96da45e3e26bdce300d394086
silvertxd <silvertxd@gmail.com>
2024-04-17 13:46:29 +0200

    fixed product card animation

--- hikikomori-frontend/components/ui/product-card.tsx
+++ hikikomori-frontend/components/ui/product-card.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import type { ApiProduct } from '@/lib/definitions';
+import { useHover } from '@/hooks/use-hover';
+import type { ApiProduct } from '@/util/definitions';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRef } from 'react';
@@ -11,23 +12,17 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-	const hoverRef = useRef<HoverEffect | null>(null);
+	const buttonHoverRef = useHover();
 
 	return (
-		<Link href={`/product/${product.id}`}>
-			<div
-				className="font-raleway item group flex flex-col items-center w-72 dark:bg-zinc-900 dark:bg-opacity-30 rounded-lg py-2 px-5"
-				ref={ref => {
-					if (hoverRef.current) return;
-					hoverRef.current = new HoverEffect(ref!);
-				}}
-			>
+		<Link href={`/product/${product.id}`} className="card" ref={buttonHoverRef}>
+			<div className="font-raleway item group flex flex-col items-center w-72 rounded-lg py-2 px-5">
 				<Image
 					width={250}
 					height={250}
 					src={`http://localhost:8000/storage/${product.images[0].image_path}`}
 					alt={product.name}
-					className="h-64 w-64 max-w-[250px] object-contain transition-all delay-300"
+					className="h-64 w-64 max-w-[250px] object-contain"
 					unoptimized={true}
 				/>
 				<h3 className="mt-10 text-l group-hover:text-red-700 dark:text-zinc-400">
