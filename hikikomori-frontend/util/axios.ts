a3298d76929e83c893d76c364e4ebe48ec6ba69f
silvertxd <silvertxd@gmail.com>
2024-04-17 13:48:05 +0200

    moved utility files to new folder

---
+++ hikikomori-frontend/util/axios.ts
@@ -0,0 +1,13 @@
+import Axios from 'axios';
+
+const axios = Axios.create({
+	baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+	headers: {
+		'X-Requested-With': 'XMLHttpRequest',
+		'Content-Type': 'multipart/form-data',
+	},
+	withCredentials: true,
+	withXSRFToken: true,
+});
+
+export default axios;
