d906dee403db6b9302c3f7ad1c878acfa53e9c2e
silvertxd <silvertxd@gmail.com>
2024-04-21 14:05:07 +0200

    add login middleware

--- hikikomori-frontend/components/dashboard-content.tsx
+++ hikikomori-frontend/components/dashboard-content.tsx
@@ -1,9 +1,33 @@
 'use client';
 import { AuroraBackground } from '@/components/ui/aurora-background';
+import { useAuth } from '@/hooks/auth';
 import { motion } from 'framer-motion';
+import { useSearchParams } from 'next/navigation';
 import AddProductForm from './add-product-form';
+import { useEffect, useState } from 'react';
 
 export default function DashboardContent() {
+	const searchParams = useSearchParams();
+	const [status, setStatus] = useState<string | null>(null);
+	const [errors, setErrors] = useState<{ [key: string]: string[] }>({});
+	const { user } = useAuth({
+		middleware: 'auth',
+		redirectIfAuthenticated: '/login',
+	});
+
+	useEffect(() => {
+		const resetParam = searchParams.get('reset');
+		if (
+			resetParam &&
+			resetParam.length > 0 &&
+			Object.keys(errors).length === 0
+		) {
+			setStatus(atob(resetParam));
+		} else {
+			setStatus(null);
+		}
+	});
+
 	return (
 		<AuroraBackground>
 			<motion.div
