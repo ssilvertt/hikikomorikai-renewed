7f7aa3bc78ea9c179db631fc473eddc37e70dca7
silvertxd <silvertxd@gmail.com>
2024-04-21 14:06:56 +0200

    delete ref on product card

--- hikikomori-frontend/components/ui/product-card.tsx
+++ hikikomori-frontend/components/ui/product-card.tsx
@@ -4,18 +4,14 @@ import { useHover } from '@/hooks/use-hover';
 import type { ApiProduct } from '@/util/definitions';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRef } from 'react';
-import { HoverEffect } from './HoverEffect';
 
 interface ProductCardProps {
 	product: ApiProduct;
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-	const buttonHoverRef = useHover();
-
 	return (
-		<Link href={`/product/${product.id}`} className="card" ref={buttonHoverRef}>
+		<Link href={`/product/${product.id}`} className="card">
 			<div className="font-raleway item group flex flex-col items-center w-72 rounded-lg py-2 px-5">
 				<Image
 					width={250}
