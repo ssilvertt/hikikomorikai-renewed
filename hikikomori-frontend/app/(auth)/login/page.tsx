e2467ddfac26414373f277fa79ef7bd4dbc86932
silvertxd <silvertxd@gmail.com>
2024-04-21 14:01:02 +0200

    add beautiful login form

--- hikikomori-frontend/app/(auth)/login/page.tsx
+++ hikikomori-frontend/app/(auth)/login/page.tsx
@@ -3,6 +3,31 @@
 import { useAuth } from '@/hooks/auth';
 import { useSearchParams } from 'next/navigation';
 import { FormEvent, useEffect, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+	email: z.string().min(2, {
+		message: 'Email must be at least 2 characters.',
+	}),
+	password: z.string().min(2, {
+		message: 'Password must be at least 2 characters.',
+	}),
+});
+
 export default function Login() {
 	const searchParams = useSearchParams();
 
@@ -17,6 +42,14 @@ export default function Login() {
 	const [errors, setErrors] = useState<{ [key: string]: string[] }>({});
 	const [status, setStatus] = useState<string | null>(null);
 
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	});
+
 	useEffect(() => {
 		const resetParam = searchParams.get('reset');
 		if (
@@ -30,36 +63,51 @@ export default function Login() {
 		}
 	});
 
-	const submitForm = async (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-
-		login({
-			email,
-			password,
+	async function onSubmit(values: z.infer<typeof formSchema>) {
+		const emailForm: string = values.email;
+		const passwordForm: string = values.password;
+		await login({
+			email: emailForm,
+			password: passwordForm,
 			remember: shouldRemember,
 			setErrors,
 			setStatus,
 		});
-	};
-
-	if (user) {
 	}
 
 	return (
-		<form onSubmit={submitForm} className="mt-20">
-			<input
-				type="email"
-				placeholder="Email"
-				value={email}
-				onChange={e => setEmail(e.target.value)}
-			/>
-			<input
-				type="password"
-				placeholder="Password"
-				value={password}
-				onChange={e => setPassword(e.target.value)}
-			/>
-			<button type="submit">Login</button>
-		</form>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-20 mx-auto w-[350px]">
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormDescription>Enter email</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Password</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormDescription>Enter password</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
 	);
 }
